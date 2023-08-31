c5ef2a68b4468451c963728945b3bb6afb95c7ef
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2023-08-31 17:06:09 +0300

    complete maybe pagination

--- src/components/ui/pagination/pagination.stories.tsx
+++ src/components/ui/pagination/pagination.stories.tsx
@@ -12,17 +12,18 @@ const meta = {
 
 export default meta
 
-export const Default = () => <Pagination page={2} totalPages={10} setPage={() => {}} />
+export const Default = () => <Pagination page={2} total={10} setPage={() => {}} />
 
 export function Primary() {
   let [pageNumber, setPageNumber] = useState(1)
 
-  let total = 50
+  let total = 155
 
   return (
     <>
-      <Pagination page={pageNumber} totalPages={total} setPage={setPageNumber} />
-      <h1>{pageNumber}</h1>
+      <Pagination page={pageNumber} total={total} setPage={setPageNumber} />
+      {<p>pageNumber : {pageNumber}</p>}
+      {<p>total : {total}</p>}
     </>
   )
 }
--- src/components/ui/pagination/pagination.tsx
+++ src/components/ui/pagination/pagination.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 import { Typography } from '..'
 import { SelectComponent } from '../select'
@@ -7,12 +7,14 @@ import s from './pagination.module.scss'
 
 export type PaginationType = {
   page: number
-  totalPages: number
-  count?: number
+  total: number
   setPage: (page: number) => void
 }
 
-export const Pagination: FC<PaginationType> = ({ page, totalPages, count = 10, setPage }) => {
+export const Pagination: FC<PaginationType> = ({ page, total, setPage }) => {
+  let [totalSeparated, setSeparated] = useState('10')
+
+  let totalPages = Math.ceil(total / +totalSeparated)
   const pageNumbers = []
 
   for (let index = 1; index < totalPages + 1; index++) {
@@ -20,7 +22,7 @@ export const Pagination: FC<PaginationType> = ({ page, totalPages, count = 10, s
   }
 
   const prevPage = () => {
-    page >= 1 && setPage(page - 1)
+    page > 1 && setPage(page - 1)
   }
 
   const nextPage = () => {
@@ -39,12 +41,14 @@ export const Pagination: FC<PaginationType> = ({ page, totalPages, count = 10, s
           page >= totalPages - 5 ? totalPages : page + 1
         )
 
+  const pageSliceFinally = totalPages > 5 ? pageSlice : pageNumbers
+
   return (
     <div className={s.pagination}>
       <span onClick={prevPage} className={s.arrow}>
         {'<  '}
       </span>
-      {page > 4 && (
+      {page > 4 && pageNumbers.length > 5 && (
         <>
           <span className={s.element} onClick={() => newPage(1)}>
             {1}
@@ -52,16 +56,20 @@ export const Pagination: FC<PaginationType> = ({ page, totalPages, count = 10, s
           {'...  '}
         </>
       )}
-      {pageSlice.map(el => {
+      {pageSliceFinally.map(el => {
         return (
-          <span key={el} onClick={() => newPage(el)} className={s.element}>
+          <span
+            key={el}
+            onClick={() => newPage(el)}
+            className={`${s.element} ${page === el && s.elementActive}`}
+          >
             <Typography variant="body1" className={s.text}>
               {el}
             </Typography>
           </span>
         )
       })}
-      {page < totalPages - 3 && (
+      {page < totalPages - 3 && pageNumbers.length > 5 && (
         <>
           {' ...'}
           <span onClick={() => newPage(totalPages)} className={s.element}>
@@ -72,15 +80,25 @@ export const Pagination: FC<PaginationType> = ({ page, totalPages, count = 10, s
       <span onClick={nextPage} className={s.arrow}>
         {'  >'}
       </span>
-      {totalPages > 10 && (
-        <>
-          <span className={s.textSelectShow}>Показать</span>
-          <div className={s.select}>
-            <SelectComponent />
-          </div>
-          <span className={s.textListSelect}>На странице</span>
-        </>
-      )}
+
+      <span className={s.textSelectShow}>Показать</span>
+      <div className={s.select}>
+        <SelectComponent
+          value={totalSeparated}
+          options={[
+            { value: '10', label: '10' },
+            { value: '20', label: '20' },
+            { value: '30', label: '35' },
+            { value: '50', label: '50' },
+            { value: '100', label: '100' },
+          ]}
+          onChange={(e: any) => {
+            setSeparated(e)
+            newPage(1)
+          }}
+        />
+      </div>
+      <span className={s.textListSelect}>На странице</span>
     </div>
   )
 }
